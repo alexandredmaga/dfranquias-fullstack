6579ebe31a66db4964791643f475e29ba7fa226c
alexandredmaga <alexandre.dm2020@gmail.com>
2023-04-22 11:22:20 -0300

    feat: add form

--- src/Controller/GadoController.php
+++ src/Controller/GadoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\GadoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,5 +16,17 @@ class GadoController extends AbstractController
         $data['titulo'] = 'Gerenciar bovinos';
 
         return $this->render('gado/index.html.twig', $data);
+    }
+
+    #[Route('/gado/adicionar', name: 'gado_adicionar')]
+    public function adicionar(): Response
+    {
+
+        $form = $this->createForm(GadoType::class);
+        
+        $data['titulo'] = 'Adicionar novo gado';
+        $data['form'] = $form;
+
+        return $this->renderForm('gado/form.html.twig', $data);
     }
 }
---
+++ src/Form/GadoType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class GadoType extends AbstractType
+{
+		
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('codigo', TextType::class, [
+				'label' => 'Código do gado: ',
+				'attr' => ['placeholder' => 'Codigo']
+			])
+			->add('leite', TextType::class, [
+				'label' => 'Leite por semana: ', 
+				'attr' => ['placeholder' => 'Litros de leite produzido na semana']
+			])
+			->add('racao', TextType::class, [
+				'label' => 'Ração ingerida por semana: ',
+				'attr' => ['placeholder' => 'Ração em quilos ingerida por semana']
+			])
+			->add('peso', TextType::class, [
+				'label' => 'Peso do Gado em quilos: ', 
+				'attr' => ['placeholder' => 'Peso do gado em quilos']
+			])
+			->add('nascimento', DateType::class, [
+				'label' => 'Data de nascimento do gado',
+				'html5' => false,
+				'format' => 'dd/MM/yyyy',
+                'widget' => 'single_text',
+			])
+			->add('Salvar', SubmitType::class);
+	}
+
+}
