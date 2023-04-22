3c29df5c60530e5312b2345e8360e05d28d20724
alexandredmaga <alexandre.dm2020@gmail.com>
2023-04-22 12:48:56 -0300

    feat: crud editar/excluir/listar

--- src/Controller/GadoController.php
+++ src/Controller/GadoController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Gado;
 use App\Form\GadoType;
+use App\Repository\GadoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,9 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class GadoController extends AbstractController
 {
     #[Route('/gado', name: 'gado')]
-    public function index(): Response
+    public function index(GadoRepository $gadoRepository): Response
     {
 
+        $gados = $gadoRepository->findAll();
+        $data['gados'] = $gados;
         $data['titulo'] = 'Gerenciar bovinos';
 
         return $this->render('gado/index.html.twig', $data);
@@ -41,4 +44,36 @@ class GadoController extends AbstractController
 
         return $this->renderForm('gado/form.html.twig', $data);
     }
+
+    #[Route('/gado/editar/{id}', name: 'gado_editar')]
+    public function editar($id, Request $request, EntityManagerInterface $em, GadoRepository $gadoRepository): Response
+    {
+
+        $gado = $gadoRepository->find($id);
+        $form = $this->createForm(GadoType::class, $gado);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $em->flush();
+        }
+
+        $data['titulo'] = 'Editar gado';
+        $data['form'] = $form;
+
+        return $this->renderForm('gado/form.html.twig', $data);
+    }
+
+    #[Route('/gado/excluir/{id}', name: 'gado_excluir')]
+    public function excluir($id, EntityManagerInterface $em, GadoRepository $gadoRepository): Response
+    {
+
+        $gado = $gadoRepository->find($id);
+
+        $em->remove($gado);
+        $em->flush();
+
+        return $this->redirectToRoute('gado');
+    }
+
 }
